5f2b0ccf061c33597fc9bac74912d0b7cb958e44
Pietro Henrique <pietro_henrique4@hotmail.com>
2023-10-28 21:50:02 -0300

    Update  JsonStreamTokenizer

--- src/utils/JsonStreamTokenizer.spec.ts
+++ src/utils/JsonStreamTokenizer.spec.ts
@@ -2,8 +2,6 @@ import { JsonStreamTokenizer } from "./JsonStreamTokenizer";
 import { describe, it, expect } from "vitest";
 import fs from "fs";
 
-
-
 describe("JsonStreamTokenizer", () => {
   it("should parse a simple JSON object", () => {
     const parser = new JsonStreamTokenizer();
@@ -33,12 +31,13 @@ describe("JsonStreamTokenizer", () => {
     expect(tokens).toEqual([
       { type: "BEGIN_OBJECT", depth: 0 },
       { type: "BEGIN_OBJECT", key: "glossary", depth: 1 },
-      { type: "string", value: "example glossary", depth: 1, key: "title" },
-      { type: "string", value: 2, depth: 1, key: "p2" },
-      { type: "BEGIN_ARRAY", key: "empty", depth: 1 },
-      { type: "string", value: "empty1", depth: 1, key: 0 },
-      { type: "string", value: "empty2", depth: 1, key: 1 },
-      { type: "END_ARRAY", depth: 1 },
+      { type: "string", value: "example glossary", depth: 2, key: "title" },
+      { type: "number", value: 2, depth: 2, key: "p2" },
+      { type: "BEGIN_ARRAY", key: "empty", depth: 2 },
+      { type: "string", value: "empty1", depth: 3, key: 0 },
+      { type: "string", value: "empty2", depth: 3, key: 1 },
+      { type: "END_ARRAY", depth: 2 },
+      { type: "END_OBJECT", depth: 1 },
       { type: "END_OBJECT", depth: 0 },
     ]);
   });
@@ -77,13 +76,37 @@ describe("JsonStreamTokenizer", () => {
       parser.processChar(char);
     }
 
+    expect(tokens).toEqual([
+      { type: "BEGIN_OBJECT", depth: 0 },
+      { type: "string", value: "John", depth: 1, key: "name" },
+      { type: "number", value: 30, depth: 1, key: "age" },
+      { type: "string", value: "New York", depth: 1, key: "city" },
+      { type: "BEGIN_ARRAY", key: "pets", depth: 1 },
+      { type: "BEGIN_OBJECT", key: 0, depth: 2 },
+      { type: "string", value: "Fluffy", depth: 3, key: "name" },
+      { type: "string", value: "cat", depth: 3, key: "type" },
+      { type: "END_OBJECT", depth: 2 },
+      { type: "BEGIN_OBJECT", key: 1, depth: 2 },
+      { type: "string", value: "Fido", depth: 3, key: "name" },
+      { type: "string", value: "dog", depth: 3, key: "type" },
+      { type: "END_OBJECT", depth: 2 },
+      { type: "END_ARRAY", depth: 1 },
+      { type: "BEGIN_OBJECT", key: "address", depth: 1 },
+      { type: "string", value: "123 Main St", depth: 2, key: "street" },
+      { type: "string", value: "Anytown", depth: 2, key: "city" },
+      { type: "string", value: "CA", depth: 2, key: "state" },
+      { type: "string", value: "12345", depth: 2, key: "zip" },
+      { type: "END_OBJECT", depth: 1 },
+      { type: "END_OBJECT", depth: 0 },
+    ]);
+
     parser.end();
   });
 
   it("Should throw an error when parsing invalid JSON", () => {
     const parser = new JsonStreamTokenizer();
     const tokens: any[] = [];
-    const invalidJson =  fs.readFileSync("./__test__/invalid.json", "utf8");
+    const invalidJson = fs.readFileSync("./__test__/invalid.json", "utf8");
 
     parser.subscribe((token) => {
       tokens.push(token);
@@ -95,6 +118,120 @@ describe("JsonStreamTokenizer", () => {
       for (let char of jsonString) {
         parser.processChar(char);
       }
-    }).toThrowError("Unexpected character at start");
+      parser.end();
+    }).toThrowError("Unexpected end of input");
+  });
+
+  it("Should parse the valid JSON file pokedex.json", () => {
+    const parser = new JsonStreamTokenizer();
+    const tokens: any[] = [];
+    const validJson = fs.readFileSync("./__test__/pokedex.json", "utf8");
+
+    parser.subscribe((token) => {
+      tokens.push(token);
+    });
+
+    const jsonString = validJson;
+
+    expect(() => {
+      for (let char of jsonString) {
+        parser.processChar(char);
+      }
+      parser.end();
+    }).not.toThrowError();
+  });
+
+  it("Should parse the valid JSON file small.json", () => {
+    const parser = new JsonStreamTokenizer();
+    const tokens: any[] = [];
+    const validJson = fs.readFileSync("./__test__/small.json", "utf8");
+
+    parser.subscribe((token) => {
+      tokens.push(token);
+    });
+
+    const jsonString = validJson;
+
+    expect(() => {
+      for (let char of jsonString) {
+        parser.processChar(char);
+      }
+      parser.end();
+    }).not.toThrowError();
+  });
+
+  it("Should parse the valid JSON file nullreference.json", () => {
+    const parser = new JsonStreamTokenizer();
+    const tokens: any[] = [];
+    const validJson = fs.readFileSync("./__test__/nullreference.json", "utf8");
+
+    parser.subscribe((token) => {
+      tokens.push(token);
+    });
+
+    const jsonString = validJson;
+
+    expect(() => {
+      for (let char of jsonString) {
+        parser.processChar(char);
+      }
+      parser.end();
+    }).not.toThrowError();
+  });
+
+  it("Should parse the valid JSON file startwitharray.json", () => {
+    const parser = new JsonStreamTokenizer();
+    const tokens: any[] = [];
+    const validJson = fs.readFileSync("./__test__/startwitharray.json", "utf8");
+
+    parser.subscribe((token) => {
+      tokens.push(token);
+    });
+
+    expect(() => {
+      for (let char of validJson) {
+        parser.processChar(char);
+      }
+      parser.end();
+    }).not.toThrowError();
+  });
+
+  it("Should parse the valid JSON file large.json", () => {
+    const parser = new JsonStreamTokenizer();
+    const tokens: any[] = [];
+    const validJson = fs.readFileSync("./__test__/large.json", "utf8");
+
+    expect(() => {
+      JSON.parse(validJson);
+    }).not.toThrowError();
+
+    parser.subscribe((token) => {
+      tokens.push(token);
+    });
+
+    const jsonString = validJson;
+
+    expect(() => {
+      for (let char of jsonString) {
+        parser.processChar(char);
+      }
+      parser.end();
+    }).not.toThrowError();
+  });
+
+  it("Should parse the valid JSON file giant.json", () => {
+    const parser = new JsonStreamTokenizer();
+    const validJson = fs.readFileSync("./__test__/giant.json", "utf8");
+
+ 
+
+    const jsonString = validJson;
+
+    expect(() => {
+      for (let char of jsonString) {
+        parser.processChar(char);
+      }
+      parser.end();
+    }).not.toThrowError();
   });
 });
--- src/utils/JsonStreamTokenizer.ts
+++ src/utils/JsonStreamTokenizer.ts
@@ -3,17 +3,22 @@ enum State {
   EXPECT_STRING_KEY_OR_END,
   EXPECT_COLON,
   EXPECT_VALUE,
-  IN_STRING,
+  IN_STRING_VALUE,
+  IN_NUMBER,
   IN_ESCAPE,
   IN_ARRAY,
   IN_OBJECT,
+  IN_KEY,
+  IN_FALSE,
+  IN_TRUE,
+  IN_NULL,
 }
 
-const TokenDataType =
-  "BEGIN_OBJECT" || "END_OBJECT" || "BEGIN_ARRAY" || "END_ARRAY" || "string";
+type TokenDataType =
+  "BEGIN_OBJECT" | "END_OBJECT" | "BEGIN_ARRAY" | "END_ARRAY" | "STRING" | "NUMBER"| "BOOLEAN" | "NULL" ;
 
 interface Token {
-  type: string;
+  type: TokenDataType;
   key?: string | number;
   value?: any;
   depth: number;
@@ -22,13 +27,13 @@ interface Token {
 type TokenListener = (token: Token) => void;
 
 export class JsonStreamTokenizer {
-  private state: State = State.START;
+  private state: State[] = [State.START];
   private listeners: TokenListener[] = [];
   private buffer: string = "";
   private depth: number = 0;
-  private arrayIndices: { [key: string]: number } = {};
+  private arrayIndices: number[] = [];
   private keyStack: (string | number)[] = [];
-  private tokens: Token[] = [];
+  // private tokens: Token[] = [];
 
   subscribe(listener: TokenListener) {
     this.listeners.push(listener);
@@ -36,16 +41,14 @@ export class JsonStreamTokenizer {
 
   private emit(token: Token) {
     token.depth = this.depth;
-    if (this.keyStack.length > 0) {
-      token.key = this.getCurrentKey();
-    }
+    // this.tokens.push(token);
     for (const listener of this.listeners) {
       listener(token);
     }
   }
 
   end() {
-    if (this.state !== State.START) {
+    if (this.state.length > 0 || this.depth > 0) {
       throw new Error("Unexpected end of input");
     }
   }
@@ -55,88 +58,220 @@ export class JsonStreamTokenizer {
   }
 
   private getNextArrayIndex() {
-    const key = this.keyStack.join(".");
-    return (this.arrayIndices[key] = (this.arrayIndices[key] || 0) + 1);
+    const key = this.arrayIndices.pop();
+    if (key === undefined) {
+      throw new Error("No array index found");
+    }
+    this.arrayIndices.push(key + 1);
+    return key;
   }
 
   processChunk(chunk: string) {
     for (let char of chunk) {
-      this.processChar(char);
+      try {
+        this.processChar(char);
+      } catch (err) {
+        throw err;
+      }
     }
   }
 
   public processChar(char: string) {
-    switch (this.state) {
+    switch (this.state[this.state.length - 1]) {
+      case State.IN_NUMBER:
+        if (/[0-9.]/.test(char)) {
+          this.buffer += char;
+        } else {
+          this.state.pop();
+          const value = Number(this.buffer);
+          this.buffer = "";
+          if (this.keyStack.length > 0) {
+            this.emit({
+              type: "NUMBER",
+              value,
+              depth: this.depth,
+              key: this.keyStack.pop(),
+            });
+          } else {
+            this.emit({
+              type: "NUMBER",
+              value,
+              depth: this.depth - 1,
+              key: this.getCurrentKey(),
+            });
+          }
+          this.processChar(char);
+        }
+        break;
+
       case State.START:
         if (char === "{") {
-          this.state = State.EXPECT_STRING_KEY_OR_END;
+          this.state.pop();
+          this.state.push(State.EXPECT_STRING_KEY_OR_END);
           this.emit({ type: "BEGIN_OBJECT", depth: this.depth });
           this.depth++;
         } else if (char === "[") {
-          this.state = State.IN_ARRAY;
+          this.state.pop();
+          this.state.push(State.IN_ARRAY);
+          this.arrayIndices.push(0);
+          this.keyStack.push(this.getNextArrayIndex());
           this.emit({ type: "BEGIN_ARRAY", depth: this.depth });
           this.depth++;
-        } else if (char.trim() !== "") {
+        } else if (char !== "") {
           throw new Error("Unexpected character at start");
         }
         break;
+
       case State.EXPECT_STRING_KEY_OR_END:
         if (char === "}") {
-          this.state = State.START;
-          this.emit({ type: "END_OBJECT", depth: this.depth });
+          this.state.pop();
           this.depth--;
-          this.keyStack.pop();
+          this.emit({ type: "END_OBJECT", depth: this.depth });
         } else if (char === '"') {
-          this.state = State.IN_STRING;
+          this.state.push(State.IN_KEY);
           this.buffer = "";
+        } else if (char === "]") {
+          throw new Error("Unexpected character");
         }
         break;
+
+      case State.IN_KEY:
+        if (char === '"') {
+          this.state.pop();
+          this.state.push(State.EXPECT_COLON);
+          const key = this.buffer;
+          this.buffer = "";
+          this.keyStack.push(key);
+        } else {
+          this.buffer += char;
+        }
+        break;
+
       case State.EXPECT_COLON:
         if (char === ":") {
-          this.state = State.EXPECT_VALUE;
+          this.state.pop();
+          this.state.push(State.EXPECT_VALUE);
         }
         break;
+
       case State.EXPECT_VALUE:
         if (char === "{") {
-          this.state = State.EXPECT_STRING_KEY_OR_END;
-          this.emit({ type: "BEGIN_OBJECT", depth: this.depth });
+          this.state.pop();
+          this.state.push(State.EXPECT_STRING_KEY_OR_END);
+          this.emit({
+            type: "BEGIN_OBJECT",
+            depth: this.depth,
+            key: this.keyStack.pop(),
+          });
           this.depth++;
+          if (this.state[this.state.length - 2] === State.IN_ARRAY) {
+            this.keyStack.push(this.getNextArrayIndex());
+          }
         } else if (char === "[") {
-          this.state = State.IN_ARRAY;
-          this.emit({ type: "BEGIN_ARRAY", depth: this.depth });
+          this.state.pop();
+          this.state.push(State.IN_ARRAY);
+          this.emit({
+            type: "BEGIN_ARRAY",
+            depth: this.depth,
+            key: this.keyStack.pop(),
+          });
+          if (this.state[this.state.length - 2] === State.IN_ARRAY) {
+            this.keyStack.push(this.getNextArrayIndex());
+          }
+          this.arrayIndices.push(0);
+          this.keyStack.push(this.getNextArrayIndex());
           this.depth++;
         } else if (char === '"') {
-          this.state = State.IN_STRING;
+          this.state.pop();
+          this.state.push(State.IN_STRING_VALUE);
           this.buffer = "";
         } else if (/[0-9-]/.test(char)) {
-          this.state = State.IN_STRING;
+          this.state.pop();
+          this.state.push(State.IN_NUMBER);
           this.buffer = char;
         } else if (char === "t") {
-          this.state = State.IN_STRING;
-          this.buffer = "true";
+          this.state.pop();
+          this.state.push(State.IN_TRUE);
+          this.buffer = char;
         } else if (char === "f") {
-          this.state = State.IN_STRING;
-          this.buffer = "false";
+          this.state.pop();
+          this.state.push(State.IN_FALSE);
+          this.buffer = char;
         } else if (char === "n") {
-          this.state = State.IN_STRING;
-          this.buffer = "null";
+          this.state.pop();
+          this.state.push(State.IN_NULL);
+          this.buffer = char;
         }
         break;
-      case State.IN_STRING:
+
+      case State.IN_TRUE:
+        this.buffer += char;
+        if ("true".includes(this.buffer)) {
+          if (this.buffer === "true") {
+            this.state.pop();
+            this.emit({
+              type: "BOOLEAN",
+              value: true,
+              depth: this.depth,
+              key: this.keyStack.pop(),
+            });
+          }
+        } else {
+          throw new Error("Unexpected character");
+        }
+        break;
+
+      case State.IN_FALSE:
+        this.buffer += char;
+        if ("false".includes(this.buffer)) {
+          if (this.buffer === "false") {
+            this.state.pop();
+            this.emit({
+              type: "BOOLEAN",
+              value: false,
+              depth: this.depth,
+              key: this.keyStack.pop(),
+            });
+          }
+        } else {
+          throw new Error("Unexpected character");
+        }
+        break;
+
+      case State.IN_NULL:
+        this.buffer += char;
+        if ("null".includes(this.buffer)) {
+          if (this.buffer === "null") {
+            this.state.pop();
+            this.emit({
+              type: "NULL",
+              value: null,
+              depth: this.depth,
+              key: this.keyStack.pop(),
+            });
+          }
+        } else {
+          throw new Error("Unexpected character");
+        }
+        break;
+
+      case State.IN_STRING_VALUE:
         if (char === "\\") {
-          this.state = State.IN_ESCAPE;
+          this.state.push(State.IN_ESCAPE);
         } else if (char === '"') {
-          this.state =
-            this.keyStack.length > 0
-              ? State.EXPECT_COLON
-              : State.EXPECT_STRING_KEY_OR_END;
+          this.state.pop();
           const value = this.buffer;
           this.buffer = "";
           if (this.keyStack.length > 0) {
-            this.emit({ type: "string", value, depth: this.depth });
+            this.emit({
+              type: "STRING",
+              value,
+              depth: this.depth,
+              key: this.keyStack.pop(),
+            });
           } else {
             this.emit({
-              type: "string",
+              type: "STRING",
               value,
               depth: this.depth - 1,
               key: this.getCurrentKey(),
@@ -146,40 +281,54 @@ export class JsonStreamTokenizer {
           this.buffer += char;
         }
         break;
+
       case State.IN_ESCAPE:
         this.buffer += char;
-        this.state = State.IN_STRING;
+        this.state.pop();
         break;
+
       case State.IN_ARRAY:
         if (char === "]") {
-          this.state = this.depth === 1 ? State.START : State.EXPECT_VALUE;
-          this.emit({ type: "END_ARRAY", depth: this.depth });
-          this.depth--;
           this.keyStack.pop();
+          this.arrayIndices.pop();
+          this.state.pop();
+          this.emit({ type: "END_ARRAY", depth: --this.depth });
         } else if (char === "{") {
-          this.state = State.EXPECT_STRING_KEY_OR_END;
-          this.emit({ type: "BEGIN_OBJECT", depth: this.depth });
+          this.state.push(State.EXPECT_STRING_KEY_OR_END);
+          this.emit({
+            type: "BEGIN_OBJECT",
+            depth: this.depth,
+            key: this.keyStack.pop(),
+          });
           this.depth++;
         } else if (char === "[") {
-          this.state = State.IN_ARRAY;
-          this.emit({ type: "BEGIN_ARRAY", depth: this.depth });
+          this.state.push(State.IN_ARRAY);
+          this.emit({
+            type: "BEGIN_ARRAY",
+            depth: this.depth,
+            key: this.keyStack.pop(),
+          });
+          this.arrayIndices.push(0);
+          this.keyStack.push(this.getNextArrayIndex());
           this.depth++;
         } else if (char === '"') {
-          this.state = State.IN_STRING;
+          this.state.push(State.IN_STRING_VALUE);
           this.buffer = "";
         } else if (/[0-9-]/.test(char)) {
-          this.state = State.IN_STRING;
+          this.state.push(State.IN_NUMBER);
           this.buffer = char;
         } else if (char === "t") {
-          this.state = State.IN_STRING;
+          this.state.push(State.IN_STRING_VALUE);
           this.buffer = "true";
         } else if (char === "f") {
-          this.state = State.IN_STRING;
+          this.state.push(State.IN_STRING_VALUE);
           this.buffer = "false";
         } else if (char === "n") {
-          this.state = State.IN_STRING;
+          this.state.push(State.IN_STRING_VALUE);
           this.buffer = "null";
-        } else if (char.trim() !== "") {
+        } else if (char === ",") {
+          this.keyStack.push(this.getNextArrayIndex());
+        } else if (char === "}") {
           throw new Error("Unexpected character in array");
         }
         break;
