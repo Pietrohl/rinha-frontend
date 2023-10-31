7e2afff754719183ac9b45d8c106237f0008b881
Pietro Henrique <pietro_henrique4@hotmail.com>
2023-10-30 23:22:23 -0300

    finish styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Match, Switch, createEffect, createSignal,on } from "solid-js";
+import { Match, Show, Switch, createEffect, createSignal, on } from "solid-js";
 import "./App.css";
 import VirtualizedPanel from "./components/VirtualizedPanel";
 import { createMutable } from "solid-js/store";
@@ -16,15 +16,15 @@ function App() {
 
   // parhaps change this so that receive an array of tokens?
   parser.subscribe((token) => {
-    if (token) {
+    if (token && !list.error) {
       if (token.type === "error") {
+        list.items = [];
         list.error = "Invalid Json!";
         return;
       }
       list.items.push(token);
     }
   });
-
 
   createEffect(
     on(
@@ -39,6 +39,7 @@ function App() {
         }
         const stream = file.stream().getReader({ mode: "byob" });
         list.items = [];
+        list.error = "";
 
         while (true) {
           let bufferView = new Uint8Array(buffer);
@@ -72,25 +73,44 @@ function App() {
     )
   );
 
+  let inputRef: HTMLInputElement | undefined;
   return (
     <>
-      <Switch
-        fallback={
-          <div>
-            <input
-              onInput={(e) => setFileContent(() => e.currentTarget.files?.[0])}
-              type="file"
-              id="input"
-              accept=".json"
-            />
+      <Switch fallback={<div>Loading...</div>}>
+        <Match when={!(list.items.length > 0) || list.error}>
+          <div class="container">
+            <h1>JSON Tree Viewer</h1>
+            <h2 class="h3">
+              Simple JSON Viewer that runs completely on-client. No data
+              exchange
+            </h2>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  inputRef?.click();
+                }}
+              >
+                Load JSON
+              </button>
+              <input
+                style={{ display: "none" }}
+                ref={inputRef!}
+                onInput={(e) =>
+                  setFileContent(() => e.currentTarget.files?.[0])
+                }
+                type="file"
+                id="input"
+                accept=".json"
+              />
+            </div>
+            <Show when={list.error}>
+              <p class="error">Invalid file. Please load a valid JSON file. </p>
+            </Show>
           </div>
-        }
-      >
-        <Match when={list.error}>
-          <div>Error: {list.error} </div>
         </Match>
         <Match when={list.items.length > 0}>
-          <h2>{fileContent()?.name}</h2>
+          <h1 class="h2">{fileContent()?.name}</h1>
           <VirtualizedPanel list={list.items} />
         </Match>
       </Switch>
