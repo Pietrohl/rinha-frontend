b54e05abc1df8b1b491f7e9f20872552d777ecfc
Pietro Henrique <pietro_henrique4@hotmail.com>
2023-11-03 23:55:09 -0300

    add build

--- generate.js
+++ generate.js
@@ -1,19 +1,19 @@
 import path from "path";
 import renderStatic from "solid-ssr/static";
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 const PAGES = ["index"];
 const pathToServer = path.resolve(__dirname, "lib/entry-server.js");
 const pathToPublic = path.resolve(__dirname, "docs");
-const templateHTML = path.resolve(__dirname, "docs/index.html");
+const templateHTML = path.resolve(__dirname, "dist/index.html");
 
 renderStatic(
   PAGES.map(p => ({
     entry: pathToServer,
     output: path.join(pathToPublic, `${p}.html`),
-    url: p === "index" ? `/` : `/${p}`,
-    
+    url: p === "index" ? `/docs` : `/docs/${p}`,
   }))
 );
