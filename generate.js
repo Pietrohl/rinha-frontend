8f5df9d5ba5f73574a301d9c60f61422e5fc133b
Pietro Henrique <pietro_henrique4@hotmail.com>
2023-11-04 00:09:09 -0300

    Change build path

--- generate.js
+++ generate.js
@@ -7,13 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PAGES = ["index"];
 const pathToServer = path.resolve(__dirname, "lib/entry-server.js");
-const pathToPublic = path.resolve(__dirname, "docs");
+const pathToPublic = path.resolve(__dirname, "./");
 const templateHTML = path.resolve(__dirname, "dist/index.html");
 
 renderStatic(
   PAGES.map(p => ({
     entry: pathToServer,
     output: path.join(pathToPublic, `${p}.html`),
-    url: p === "index" ? `/docs` : `/docs/${p}`,
+    url: p === "index" ? `/` : `/${p}`,
   }))
 );
